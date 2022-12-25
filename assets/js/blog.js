6e08c64560a22274f28822541f772c8a093ead50
yusuf <mqardhawi9@gmail.com>
2022-12-25 20:23:39 +0800

    day5-blog-detail

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -21,7 +21,7 @@ function getData(event) {
     let title = document.getElementById("name").value
     let description = document.getElementById("description").value
     let image = document.getElementById("images").files
-    // let checkbox = document.getElementsByName("technologies")
+    let checkbox = document.getElementsByName("technologies")
 
     image = URL.createObjectURL(image[0])
 
@@ -40,6 +40,7 @@ function getData(event) {
         description,
         image,
         postedAt: new Date(),
+        checkbox
         // tech
 
     }
@@ -61,17 +62,17 @@ function showData() {
                 </div>
                 <div class="title">
                     <h2><a href="blog-detail.html" target="_blank"
-                        >${blogs[i].title}</a
+                        >${blogs[i].title} - ${getFuLLTime(blogs[i].postedAt)}</a
                       ></h2>
                 </div>
                 <div class="durasi">
-                    <p>durasi 3 bulan</p>
+                    <p>durasi ${getDistance(blogs[i].postedAt)}</p>
                 </div>
                 <div class="deskripsi">
                     <p>${blogs[i].description}</p>
                 </div>
                 <div class="technologies">
-                    <p></p>
+                    <p>${check(blogs[i].checkbox)}</p>
                 </div>
                 <div class="btn-group">
                     <button class="btn">edit</button>
@@ -81,3 +82,83 @@ function showData() {
         `
     }
 }
+
+function getFuLLTime(time) {
+    let years = time.getFullYear()
+    // let monthIndex = time.getMonth()
+    // let date = time.getDate()
+    // let hour = time.getHours()
+    // let minute = time.getMinutes()
+
+    // const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    // return ` ${date} ${month[monthIndex]} ${years} ${hour} ${minute}`
+    return ` ${years} `
+}
+
+function getDistance(time) {
+    let timePost = time
+    let timeNow = new Date()
+
+    let distance = timeNow - timePost
+
+    let ms = 1000
+
+    let day = Math.floor(distance / (ms * 60 * 60 * 24))
+    let hour = Math.floor(distance / (ms * 60 * 60))
+    let min = Math.floor(distance / (ms * 60))
+    let sec = Math.floor(distance / ms)
+
+    if(day > 0) {
+        return `${day} Days Ago`
+      } else if(hour > 0) {
+        return `${hour} Hours Ago`
+      }else if(min > 0) {
+        return `${min} minutes Ago`
+      } else if(sec > 0) {
+        return `${sec} seconds Ago`
+      }
+
+      setInterval(() => {
+        showData()
+      }, 1000)
+}
+
+function check(markedCheckbox) {
+    // let markedCheckbox = document.getElementsByName('technologies')
+  for (let checkbox of markedCheckbox) {
+    if (checkbox.checked)
+        
+      document.body.append(checkbox.value + ' ')
+  }
+
+//     let c1 = document.getElementsById("nodeJs")
+//     let c2 = document.getElementsById("nextJs")
+//     let c3 = document.getElementsById("reactJs")
+//     let c4 = document.getElementsById("ts")
+
+    
+//   if (c1.checked == true){
+//     return document.getElementById("result").innerHTML = "Node Js"
+//   } 
+//   else if (c2.checked == true){
+//     return document.getElementById("result").innerHTML = "Node Js"
+//   }
+//   else if (c3.checked == true){
+//     return document.getElementById("result").innerHTML = "Node Js"
+//   }
+//   else if (c4.checked == true){
+//     return document.getElementById("result").innerHTML = "Node Js"
+//   } else {
+//   return document.getElementById("result").innerHTML = "You have not selected anything"
+//   }
+  
+}
+
+// ${(function icon() {
+//     let string = ""
+//     for (let j = 0; j < dataProject[i].techChecked.length; j++) {
+//       string += `<li><img src="assets/img/icon/logo-${dataProject[i].techChecked[j]}.svg" alt="Item Icon"></li>`
+//     }
+//     return string
+//   })()}
