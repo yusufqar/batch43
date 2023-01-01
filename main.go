633bcc11cbda44e7bfbe84a58394b608ba20f5b8
yusuf <mqardhawi9@gmail.com>
2023-01-01 21:53:18 +0800

    day8-struct

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -13,6 +14,22 @@ import (
 var Data = map[string]interface{}{
 	"Title":   "Personal Web",
 	"IsLogin": true,
+}
+
+type Project struct {
+	Name     string
+	Post_date string
+	Description    string
+	Technologies   string
+}
+
+var Projects = []Project{
+	{
+		Name:     "Web Dumbways",
+		Post_date: "12 Jul 2021 | 22:30 WIB",
+		Description:    "Pembuatan Web",
+		Technologies:   "Next Js",
+	},
 }
 
 func main() {
@@ -25,6 +42,7 @@ func main() {
 	route.HandleFunc("/add-project", formProject).Methods("GET")
 	route.HandleFunc("/blog/{id}", blogDetail).Methods("GET")
 	route.HandleFunc("/add-project", addProject).Methods("POST")
+	route.HandleFunc("/delete-project/{id}", deleteProject).Methods("GET")
 	route.HandleFunc("/contact", contactMe).Methods("GET")
 
 	// port := 5000
@@ -48,8 +66,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	respData := map[string]interface{}{
+		"Data":  Data,
+		"Projects": Projects,
+	}
+
 	w.WriteHeader(http.StatusOK)
-	tmpl.Execute(w, Data)
+	tmpl.Execute(w, respData)
 }
 
 func formProject(w http.ResponseWriter, r *http.Request) {
@@ -93,11 +116,39 @@ func addProject(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Nama Project : " + r.PostForm.Get("name"))
-	fmt.Println("start Date : " + r.PostForm.Get("start"))
-	fmt.Println("End Date : " + r.PostForm.Get("end"))
-	fmt.Println("Description : " + r.PostForm.Get("description"))
+	// fmt.Println("Nama Project : " + r.PostForm.Get("name"))
+	// fmt.Println("start Date : " + r.PostForm.Get("start"))
+	// fmt.Println("End Date : " + r.PostForm.Get("end"))
+	// fmt.Println("Description : " + r.PostForm.Get("description"))
 	// fmt.Println("Technologies : " + r.PostForm.Get("technologies"))
+
+	name := r.PostForm.Get("name")
+	description := r.PostForm.Get("description")
+	technologies := r.PostForm.Get("technologies")
+
+	//code here
+	var newProject = Project{
+		Name:    name,
+		Post_date: time.Now().String(),
+		Description:    description,
+		Technologies:   technologies,
+	}
+
+	Projects = append(Projects, newProject)
+
+	fmt.Println(Projects)
+
+	http.Redirect(w, r, "/home", http.StatusMovedPermanently)
+}
+
+func deleteProject(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	fmt.Println(id)
+
+	Projects = append(Projects[:id], Projects[id+1:]...)
 
 	http.Redirect(w, r, "/home", http.StatusMovedPermanently)
 }
